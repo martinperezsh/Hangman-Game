63857c3f7a050d0b51c429c9858f4d8c18f53b8e
Martin Perez <martinperezsh@gmail.com>
2017-06-25 03:09:49 -0600

    created function to pick random string in array

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Game words
+var words = ['nas', 'biggie', 'tupac', 'eminem', 'raekwon', 'dylon'];
+console.log(words)
+var rand = words[Math.floor(Math.random() * words.length)];
+console.log(rand)
+
+// Get started key
+// document.onkeyup = function(start) {
+// 	document.getElementById('currentWord').innerHTML = 
+// }
+
