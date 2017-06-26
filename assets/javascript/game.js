1d582a5a9e18ece2d28fa35aec49a6544463cb96
martinperezsh <martinperezsh@gmail.com>
2017-06-26 01:40:50 -0600

    include dashes instead of letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,6 +3,13 @@ var words = ['nas', 'biggie', 'tupac', 'eminem', 'raekwon', 'dylon'];
 console.log(words)
 var rand = words[Math.floor(Math.random() * words.length)];
 console.log(rand)
+for (i = 0; i < rand.length; i++) {
+  if (rand.charAt(i) == " ") {
+    dashes += " ";
+  } else {
+    dashes += "-";
+  }
+}
 
 // Get started key
 // document.onkeyup = function(start) {
