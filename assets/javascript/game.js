8e0cc3ef73f5be9ea76b9925e01de086ae6077bc
Martin Perez <martinperezsh@gmail.com>
2017-07-01 02:35:08 -0600

    got word to appear as dashes instead of actual word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,30 @@
 // Game words
 var words = ['nas', 'biggie', 'tupac', 'eminem', 'raekwon'];
 console.log(words)
-var rand = words[Math.floor(Math.random() * words.length)];
-console.log(rand)
+
+var wins = 0;
+var lettersGuessed = [];
+var losses = 0;
+var i = 0;
+
+var random = words[Math.floor(Math.random() * words.length)];
 
   // Get started key
 document.onkeyup = function(start){
-	document.getElementById('unguessed').innerHTML =  rand;
+
+
+	if (random = 'nas') {
+		document.getElementById('chosenWord').innerHTML = "_ _ _";
+	} else if (random = 'biggie') {
+		document.getElementById('chosenWord').innerHTML = "_ _ _ _ _ _";
+	} else if (random = 'tupac') {
+		document.getElementById('chosenWord').innerHTML = "_ _ _ _ _";
+	} else if (random = 'eminem') {
+		document.getElementById('chosenWord').innerHTML = "_ _ _ _ _ _";
+	} else if (random = 'raekwon') {
+		document.getElementById('chosenWord').innerHTML = "_ _ _ _ _ _ _"
+	}
+
+	
+	
 }
