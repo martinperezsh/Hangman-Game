ffd7942d7647822a6656ca6790a560c133937395
Martin Perez <martinperezsh@gmail.com>
2017-07-01 01:51:42 -0600

    deleted stuff that was not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,13 +3,6 @@ var words = ['nas', 'biggie', 'tupac', 'eminem', 'raekwon'];
 console.log(words)
 var rand = words[Math.floor(Math.random() * words.length)];
 console.log(rand)
-for (i = 0; i < rand.length; i++) {
-  if (rand.charAt(i) == " ") {
-    dashes += " ";
-  } else {
-    dashes += "-";
-  }
-}
 
   // Get started key
 document.onkeyup = function(start){
